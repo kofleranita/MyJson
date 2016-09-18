c283b7341132770e9f7162fbc1b3ede5a42f28b9
Anita Kofler <kofleranita@hotmail.com>
2016-09-18 09:47:38 +0200

    test

---
+++ MyJson/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace MyJson
+{
+  public partial class Form1 : Form
+  {
+    List<clPerson> lstPerson = new List<clPerson>();
+    string destPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "file.txt");
+
+    public Form1()
+    {
+      InitializeComponent();
+      test();
+    }
+
+    private void test()
+    {
+      clPerson product = new clPerson();
+      product.Name = "Apple";
+      product.ExpireDate = new DateTime(2008, 12, 28);
+      product.Price = 3.99;
+      product.Size = new string[] { "Small", "Medium", "Large" };
+      lstPerson.Add(product);
+      DoSerialize();
+
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      this.lstPerson = JsonConvert.DeserializeObject<List<clPerson>>(File.ReadAllText(destPath));
+      clPerson a = new clPerson();
+      a.Name = "asf";
+      lstPerson.Add(a);
+      DoSerialize();
+    }
+
+    private void DoSerialize()
+    {
+      string output = JsonConvert.SerializeObject(lstPerson);
+      System.IO.File.WriteAllText(destPath, output);
+    }
+
+  }
+}
---
+++ MyJson/clPerson.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyJson
+{
+    class clPerson
+    {
+        private string name;
+        private DateTime expireDate;
+        private double price;
+        private string[] size;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public DateTime ExpireDate
+        {
+            get
+            {
+                return expireDate;
+            }
+
+            set
+            {
+                expireDate = value;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                price = value;
+            }
+        }
+
+        public string[] Size
+        {
+            get
+            {
+                return size;
+            }
+
+            set
+            {
+                size = value;
+            }
+        }
+
+        public void Person(string myName, DateTime myExpireDate, float myPrice, string[] mySize)
+        {
+            this.name = myName;
+            this.expireDate = myExpireDate;
+            this.price = myPrice;
+            this.size = mySize;
+        }
+    }
+
+}
